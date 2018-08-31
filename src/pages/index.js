3834abddb779f510aa329820dba70901bccf08b3
gitadr <mail@aaronroot.net>
2018-08-31 07:52:22 +0100

    removing work and notes from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Header from "../components/header";
-import Work from "../components/work";
-import Notes from "../components/notes";
 import Footer from "../components/footer";
 
 export default () => (
