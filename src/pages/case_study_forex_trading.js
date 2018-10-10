ab51c569cb361f0f46cb638ccc3b9a27f0e93313
gitadr <mail@aaronroot.net>
2018-10-10 12:56:45 +1100

    removed blank image

--- src/pages/case_study_forex_trading.js
+++ src/pages/case_study_forex_trading.js
@@ -277,11 +277,6 @@ export default () => (
             IG's aspiration to build category-defining apps.
           </p>
         </section>
-        <img
-          src={require("../images/caseStudies/caseStudy_forexTrading_overview.jpg")}
-          alt="Placeholder"
-          className={styles.caseStudy_MediumImage}
-        />
         <Author />
       </article>
     </div>
