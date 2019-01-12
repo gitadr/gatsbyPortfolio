f4ce61ac659e324399dd674d88036b63f0064384
gitadr <mail@aaronroot.net>
2019-01-12 21:06:53 +1100

    helmet test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,11 @@ import Footer from "../components/footer";
 
 export default () => (
   <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My Title</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <Header />
     <Work />
     <Notes />
