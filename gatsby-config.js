1c8c6a57c5c6450155b198103e42653dcc1c0f91
gitadr <mail@aaronroot.net>
2019-01-12 21:31:30 +1100

    correction to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,8 @@ module.exports = {
     siteUrl: "https://www.aaronroot.net",
     defaultDescription: "Aaron Root's Portfolio",
     twitterUsername: "@aaroot",
-    defaultImage: "images/favicon.ico",
+    defaultImage: "images/favicon.ico"
+  },
   plugins: [
     "gatsby-plugin-react-helmet",
     {
